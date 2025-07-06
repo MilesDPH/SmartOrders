49551cca28e56c20a46871557d062168cd04b3a9
Hugo Pedroza <hugo.pedroza@lionintel.com>
2025-07-06 17:29:30 -0600

    Orders factory and test done

--- App/database/migrations/2025_07_06_073501_create_orders_table.php
+++ App/database/migrations/2025_07_06_073501_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +12,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        //user_id
+        //total
+        //status
+        //;
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->enum('status', ['pending', 'processing', 'completed', 'cancelled']);
+            $table->foreignIdFor(User::class)->constrained('users')->noActionOnDelete();
+
             $table->timestamps();
         });
     }
@@ -22,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('orders');
     }
 };
